b61647afac110b65612739766b20f6b64a890dd3
KhalilGuedria <167778937+MedKhalilGuedria@users.noreply.github.com>
2024-05-05 23:56:53 +0200

    fixes

--- src/main/java/app/spring/config/SecurityConfiguration.java
+++ src/main/java/app/spring/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package app.spring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,6 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import app.spring.services.UserService;
 import app.spring.util.JwtRequestFilter;
 
+@Configuration
 @EnableWebSecurity
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
