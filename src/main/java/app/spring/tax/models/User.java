81cc2c2c31d475dddbf6ca9ea1b8c62edbec4895
KhalilGuedria <167778937+MedKhalilGuedria@users.noreply.github.com>
2024-05-09 15:08:59 +0200

    user structure change

--- src/main/java/app/spring/tax/models/User.java
+++ src/main/java/app/spring/tax/models/User.java
@@ -18,7 +18,15 @@ public class User implements Serializable {
     private Long id;
     private String username;
     private String password;
-    private String role ;
+    private String email;
+    public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	private String role ;
 	public User(String username, String password, String role) {
 		this.username = username;
 		this.password = password;
